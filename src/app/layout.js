9b6919fd8401770a24fd040b81786cb5ec612c1d
Vladyslav <vlad.kryklyvets9@gmail.com>
2025-09-28 21:28:18 +0300

    adding meta tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,26 @@ import "./styles/styleNull.css";
 export const metadata = {
   title: "AutoHelp",
   description: "Auto-Help cards",
+  openGraph: {
+    title: "AutoHelp",
+    description: "Auto-Help cards",
+    url: "https://auto-help.vercel.app/", // заміни на реальний URL проекту
+    siteName: "AutoHelp",
+    images: [
+      {
+        url: "https://i.postimg.cc/wxtb7BLQ/Screenshot-4.jpg",
+        width: 1200,
+        height: 627,
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "AutoHelp",
+    description: "Auto-Help cards",
+    images: ["https://i.postimg.cc/wxtb7BLQ/Screenshot-4.jpg"],
+  },
 };
 
 export default function RootLayout({ children }) {
